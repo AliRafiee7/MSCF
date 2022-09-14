52ce60a7faa4e56dd39a612815de3e086569276d
alirafiee7 <83859160+alirafiee7@users.noreply.github.com>
2022-09-14 16:19:57 +0430

    Update main.m

--- main.m
+++ main.m
@@ -6,11 +6,12 @@ noise_density = 0.9;
 l = 5; %Number of SCF Block
 
 image = imread('Lena.png');
+
 noisy_image = im2double(imnoise(image, 'salt & pepper', noise_density));
 
 tic
 denoised_image = MSCF(noisy_image, l);
 toc
 
-PSNR = psnr(denoised_image, image)
-SSIM = ssim(denoised_image, image)
+PSNR = psnr(denoised_image, image);
+SSIM = ssim(denoised_image, image);
